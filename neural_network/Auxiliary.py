19fed13afa91ab02137959ca2240df0f111983fe
Rodrigo Fernandes Bulgarelli <rodrigo196@gmail.com>
2017-09-21 14:01:56 -0300

    Update Auxiliary.py
    Correted a bug in image2vector implementation.

--- neural_network/Auxiliary.py
+++ neural_network/Auxiliary.py
@@ -43,7 +43,7 @@ def image2vector(image):
     """
 
 
-    v = image.reshape(image.shape[0]*image.shape[1]*image.shape[2])
+    v = image.reshape(image.shape[0]*image.shape[1]*image.shape[2], 1)
 
 
     return v
