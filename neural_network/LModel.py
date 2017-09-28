3f9a862386f7e6819bdde81ffe5859dd1f3e5825
Rodrigo Fernandes Bulgarelli <rodrigo196@gmail.com>
2017-09-28 15:41:25 -0300

    Litle refactor

--- neural_network/LModel.py
+++ neural_network/LModel.py
@@ -5,8 +5,9 @@ Created on Wed Sep 27 12:17:02 2017
 @author: rodrigo.bulgarelli
 """
 import numpy as np
-import h5py
+import matplotlib.pyplot as plt
 from NeuralNetworkCore import initialize_parameters_deep, L_model_forward, compute_cost, L_model_backward, update_parameters
+from Auxiliary import load_data
 
 def L_layer_model(X, Y, layers_dims, learning_rate = 0.0075, num_iterations = 3000, print_cost=False):#lr was 0.009
     """
@@ -52,21 +53,56 @@ def L_layer_model(X, Y, layers_dims, learning_rate = 0.0075, num_iterations = 30
     
     return parameters
 
-def load_data():
-    train_dataset = h5py.File('datasets/train_catvnoncat.h5', "r")
-    train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
-    train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
-
-    test_dataset = h5py.File('datasets/test_catvnoncat.h5', "r")
-    test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
-    test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
-
-    classes = np.array(test_dataset["list_classes"][:]) # the list of classes
+def predict(X, y, parameters):
+    """
+    This function is used to predict the results of a  L-layer neural network.
     
-    train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
-    test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
+    Arguments:
+    X -- data set of examples you would like to label
+    parameters -- parameters of the trained model
     
-    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
+    Returns:
+    p -- predictions for the given dataset X
+    """
+    
+    m = X.shape[1]
+    p = np.zeros((1,m))
+    
+    # Forward propagation
+    probas, caches = L_model_forward(X, parameters)
+
+    
+    # convert probas to 0/1 predictions
+    for i in range(0, probas.shape[1]):
+        if probas[0,i] > 0.5:
+            p[0,i] = 1
+        else:
+            p[0,i] = 0
+    
+    #print results
+    #print ("predictions: " + str(p))
+    #print ("true labels: " + str(y))
+    
+    return p
+
+def print_mislabeled_images(classes, X, y, p):
+    """
+    Plots images where predictions and truth were different.
+    X -- dataset
+    y -- true labels
+    p -- predictions
+    """
+    a = p + y
+    mislabeled_indices = np.asarray(np.where(a == 1))
+    plt.rcParams['figure.figsize'] = (40.0, 40.0) # set default size of plots
+    num_images = len(mislabeled_indices[0])
+    for i in range(num_images):
+        index = mislabeled_indices[1][i]
+        
+        plt.subplot(2, num_images, i + 1)
+        plt.imshow(X[:,index].reshape(64,64,3), interpolation='nearest')
+        plt.axis('off')
+        plt.title("Prediction: " + classes[int(p[0,index])].decode("utf-8") + " \n Class: " + classes[y[0,index]].decode("utf-8"))
 
 train_x_orig, train_y, test_x_orig, test_y, classes = load_data()
 
@@ -81,4 +117,12 @@ test_x = test_x_flatten/255.
 
 layers_dims = [12288, 7, 7, 5, 1]
 
-parameters = L_layer_model(train_x, train_y, layers_dims, num_iterations = 10000, print_cost = True)
+parameters = L_layer_model(train_x, train_y, layers_dims, num_iterations = 2500, print_cost = True)
+
+predictions_train = predict(train_x, train_y, parameters)
+print ('Accuracy of train set: %d' % float((np.dot(train_y,predictions_train.T) + np.dot(1-train_y,1-predictions_train.T))/float(train_y.size)*100) + '%')
+
+predictions_test = predict(test_x, test_y, parameters)
+print ('Accuracy of test set: %d' % float((np.dot(test_y,predictions_test.T) + np.dot(1-test_y,1-predictions_test.T))/float(test_y.size)*100) + '%')
+
+print_mislabeled_images(classes, test_x, test_y, predictions_test)
